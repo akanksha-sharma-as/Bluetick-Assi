9bebb5af5b1ee6a8c42563bfeb52295c86da103e
akanksha-sharma-as <aksharma94244@gmail.com>
2021-07-27 22:48:22 +0530

    initial commit

---
+++ js/site.js
@@ -0,0 +1,47 @@
+alert('Welcome to my site!!');
+
+// window.onload = () => {
+// var path = window.location.pathname;
+// var page = path.split("/").pop();
+// alert(page);
+// }
+
+// if(page=="about.html"){
+//     alert("about hai")
+//     var a = document.getElementsByClassName('item2').style.color = "orange"
+// }
+
+// var slideIndex = 1;
+
+// function showSlides() {
+//   setTimeout(showSlides, 2000); // Change image every 2 seconds
+//   var i;
+//   var slides = document.getElementsByClassName("mySlides");
+//   var dots = document.getElementsByClassName("dot");
+//   for (i = 0; i < slides.length; i++) {
+//     slides[i].style.display = "none";
+//   }
+//   slideIndex++;
+//   if (slideIndex > slides.length) {slideIndex = 1}
+//   for (i = 0; i < dots.length; i++) {
+//     dots[i].className = dots[i].className.replace(" active", "");
+//   }
+//   slides[slideIndex-1].style.display = "block";
+//   dots[slideIndex-1].className += " active";
+// }
+// showSlides();
+
+// var indexValue = 0;
+// function slideShow(){
+//   setTimeout(slideShow, 2500);
+//   var x;
+//   const img = document.querySelectorAll("img");
+//   for(x = 0; x < img.length; x++){
+//     img[x].style.display = "none";
+//   }
+//   indexValue++;
+//   if(indexValue > img.length){indexValue = 1}
+//   img[indexValue -1].style.display = "block";
+// }
+// slideShow();
+
